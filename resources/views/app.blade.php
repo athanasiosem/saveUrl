19542d3b558d648baf1b9656ec571e5603543c49
Athanasios Emmanouilidis <athanasiosem@inbox.com>
2015-07-29 22:43:09 +0300

    changed navbar background-color, added logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -33,7 +33,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="">saveUrl</a>
+				<a class="navbar-brand" href=""><img src="images/saveUrlLogo.png"></a>
 			</div>
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
